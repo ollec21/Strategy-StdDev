7869b48f6dfc76d1cb0f264d1e59f9304113fc5d
kenorb <kenorb@users.noreply.github.com>
2021-01-11 04:06:37 +0000

    Optimize Indicator params (pf=0.00; dd=0.00; tt=45788)

--- config/EURUSD_M5.h
+++ config/EURUSD_M5.h
@@ -1,11 +1,16 @@
-/*
+/**
  * @file
  * Defines default strategy parameter values for the given timeframe.
  */
 
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_StdDev_Params_M5 : StdDevParams {
-  Indi_StdDev_Params_M5() : StdDevParams(indi_stddev_defaults, PERIOD_M5) { shift = 0; }
+  Indi_StdDev_Params_M5() : StdDevParams(indi_stddev_defaults, PERIOD_M5) {
+    applied_price = (ENUM_APPLIED_PRICE)0;
+    ma_method = 2;
+    period = 0;
+    shift = 0;
+  }
 } indi_stddev_m5;
 
 // Defines strategy's parameter values for the given pair symbol and timeframe.
@@ -15,12 +20,12 @@ struct Stg_StdDev_Params_M5 : StgParams {
     lot_size = 0;
     signal_open_method = 0;
     signal_open_filter = 1;
-    signal_open_level = (float)0;
+    signal_open_level = (float)0.0;
     signal_open_boost = 0;
     signal_close_method = 0;
     signal_close_level = (float)0;
     price_stop_method = 0;
-    price_stop_level = (float)2;
+    price_stop_level = (float)1;
     tick_filter_method = 1;
     max_spread = 0;
   }
